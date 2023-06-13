056b86e2bc8fd1c364787a2cfb1787739a58e77c
sakibhossain <sakibhossainceo@gmail.com>
2023-06-13 10:22:45 -0700

    recap added

---
+++ recap.js
@@ -0,0 +1,13 @@
+/*
+Must know:
+1. var, let, const (alredy practiced)
+2. dedault parameter
+3. template string 
+    3.1: create multi-line string (``)
+    3.2: add dynamic variable (expression): `${}`
+4. Arrow function 
+    4.1: single paremeter arrow function
+    4.2: two paremeters arrow function
+    4.3: arrow function with function body.
+     And don't forget you have to return explicitly.
+*/
